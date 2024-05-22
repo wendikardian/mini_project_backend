ad61cb2a0bec46e544a1d104504e28c5a396bb4c
Fathir96 <fathiralfattahgunawan@gmail.com>
2024-05-22 20:22:03 +0800

    pesan

---
+++ fathir.php
@@ -0,0 +1,16 @@
+<?php
+$name = "fathir";    
+$age = 15;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Halo, nama saya adalah <?php echo $name; ?> dan umur saya adalah <?php echo $age; ?> tahun.
+</body>
+</html>
