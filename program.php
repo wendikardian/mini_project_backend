67317665da5b1e8985b95f97bb0140a23dc8c96e
wendikardian <wendikardian@gmail.com>
2024-05-22 19:02:55 +0700

    message

---
+++ program.php
@@ -0,0 +1,18 @@
+<?php
+$name = "Wendi";
+$age = 20;
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello My name is <?php echo $name; ?> and I am <?php echo $age; ?> years old.
+</body>
+</html>
